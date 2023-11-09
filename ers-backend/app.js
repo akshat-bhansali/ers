6e11d408942257fdfe067441b9c83d64810b6db1
akshatbhansali <akshatb2198@gmail.com>
2023-11-10 00:33:18 +0530

    preparing file structure

---
+++ ers-backend/app.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+
+const errorMiddleware = require("./middleware/error")
+
+app.use(express.json())
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Route imports
+const user = require("./routes/userRoute");
+app.use("/api/v1",user);
+
+
+//Middleware for errors
+app.use(errorMiddleware);
+
+module.exports= app;
